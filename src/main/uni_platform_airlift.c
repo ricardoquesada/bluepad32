7ddcfcedb3e0cf0137c9b8d363c4db381f517688
Ricardo Quesada <ricardoquesada@gmail.com>
2020-11-11 21:58:42 -0800

    airlift: basic spi "process request"

--- src/main/uni_platform_airlift.c
+++ src/main/uni_platform_airlift.c
@@ -77,123 +77,152 @@ static int spi_transfer(uint8_t out[], uint8_t in[], size_t len) {
   return (slvTrans.trans_len / 8);
 }
 
-static int request_get_fw_version(const uint8_t command[], uint8_t response[])
-{
-  response[2] = 1; // number of parameters
-  response[3] = sizeof(FIRMWARE_VERSION); // parameter 1 length
+static int request_get_fw_version(const uint8_t command[], uint8_t response[]) {
+  response[2] = 1;                         // number of parameters
+  response[3] = sizeof(FIRMWARE_VERSION);  // parameter 1 length
 
   memcpy(&response[4], FIRMWARE_VERSION, sizeof(FIRMWARE_VERSION));
 
   return 5 + sizeof(FIRMWARE_VERSION);
 }
 
-static int request_connected_gamepads(const uint8_t command[], uint8_t response[])
-{
-  response[2] = 1; // number of parameters
-  response[3] = 1; // parameter 1 length
-  response[4] = 1; // FIXME: hardcoded one connected gamepad
+static int request_connected_gamepads(const uint8_t command[],
+                                      uint8_t response[]) {
+  response[2] = 1;  // number of parameters
+  response[3] = 1;  // parameter 1 length
+  response[4] = 1;  // FIXME: hardcoded one connected gamepad
 
   return 6;
 }
 
-static int request_gamepad_data(const uint8_t command[], uint8_t response[])
-{
-  response[2] = 1; // number of parameters
-  response[3] = sizeof(_gamepad0); // parameter 1 lenght
+static int request_gamepad_data(const uint8_t command[], uint8_t response[]) {
+  response[2] = 1;                  // number of parameters
+  response[3] = sizeof(_gamepad0);  // parameter 1 lenght
   memcpy(&response[4], &_gamepad0, sizeof(_gamepad0));
 
   return 5 + sizeof(_gamepad0);
 }
 
-typedef int (*CommandHandlerType)(const uint8_t command[], uint8_t response[]);
-const CommandHandlerType commandHandlers[] = {
-  // 0x00 -> 0x0f
-  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+typedef int (*command_handler_t)(const uint8_t command[],
+                                 uint8_t response[] /* out */);
+const command_handler_t command_handlers[] = {
+    // 0x00 -> 0x0f
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL,
 
-  // 0x10 -> 0x1f
-  NULL, // setNet
-  NULL, // setPassPhrase,
-  NULL, // setKey,
-  NULL,
-  NULL, // setIPconfig,
-  NULL, // setDNSconfig,
-  NULL, // setHostname,
-  NULL, // setPowerMode,
-  NULL, // setApNet,
-  NULL, // setApPassPhrase,
-  NULL, // setDebug,
-  NULL, // getTemperature,
-  NULL, NULL, NULL, NULL,
+    // 0x10 -> 0x1f
+    NULL,  // setNet
+    NULL,  // setPassPhrase,
+    NULL,  // setKey,
+    NULL,
+    NULL,  // setIPconfig,
+    NULL,  // setDNSconfig,
+    NULL,  // setHostname,
+    NULL,  // setPowerMode,
+    NULL,  // setApNet,
+    NULL,  // setApPassPhrase,
+    NULL,  // setDebug,
+    NULL,  // getTemperature,
+    NULL, NULL, NULL, NULL,
 
-  // 0x20 -> 0x2f
-  NULL, // getConnStatus,
-  NULL, // getIPaddr,
-  NULL, // getMACaddr,
-  NULL, // getCurrSSID,
-  NULL, // getCurrBSSID,
-  NULL, // getCurrRSSI,
-  NULL, // getCurrEnct,
-  NULL, // scanNetworks,
-  NULL, // startServerTcp,
-  NULL, // getStateTcp,
-  NULL, // dataSentTcp,
-  NULL, // availDataTcp,
-  NULL, // getDataTcp,
-  NULL, // startClientTcp,
-  NULL, // stopClientTcp,
-  NULL, // getClientStateTcp,
+    // 0x20 -> 0x2f
+    NULL,  // getConnStatus,
+    NULL,  // getIPaddr,
+    NULL,  // getMACaddr,
+    NULL,  // getCurrSSID,
+    NULL,  // getCurrBSSID,
+    NULL,  // getCurrRSSI,
+    NULL,  // getCurrEnct,
+    NULL,  // scanNetworks,
+    NULL,  // startServerTcp,
+    NULL,  // getStateTcp,
+    NULL,  // dataSentTcp,
+    NULL,  // availDataTcp,
+    NULL,  // getDataTcp,
+    NULL,  // startClientTcp,
+    NULL,  // stopClientTcp,
+    NULL,  // getClientStateTcp,
 
-  // 0x30 -> 0x3f
-  NULL, // disconnect,
-  NULL,
-  NULL, // getIdxRSSI,
-  NULL, // getIdxEnct,
-  NULL, // reqHostByName,
-  NULL, // getHostByName,
-  NULL, // startScanNetworks,
-  request_get_fw_version, // getFwVersion,
-  NULL,
-  NULL, // sendUDPdata,
-  NULL, // getRemoteData,
-  NULL, // getTime,
-  NULL, // getIdxBSSID,
-  NULL, // getIdxChannel,
-  NULL, // ping,
-  NULL, // getSocket,
+    // 0x30 -> 0x3f
+    NULL,  // disconnect,
+    NULL,
+    NULL,                    // getIdxRSSI,
+    NULL,                    // getIdxEnct,
+    NULL,                    // reqHostByName,
+    NULL,                    // getHostByName,
+    NULL,                    // startScanNetworks,
+    request_get_fw_version,  // getFwVersion,
+    NULL,
+    NULL,  // sendUDPdata,
+    NULL,  // getRemoteData,
+    NULL,  // getTime,
+    NULL,  // getIdxBSSID,
+    NULL,  // getIdxChannel,
+    NULL,  // ping,
+    NULL,  // getSocket,
 
-  // 0x40 -> 0x4f
-  NULL, // setClientCert,
-  NULL, // setCertKey,
-  NULL, NULL,
-  NULL, // sendDataTcp,
-  NULL, // getDataBufTcp,
-  NULL, // insertDataBuf,
-  NULL, NULL, NULL,
-  NULL, // wpa2EntSetIdentity,
-  NULL, // wpa2EntSetUsername,
-  NULL, // wpa2EntSetPassword,
-  NULL, // wpa2EntSetCACert,
-  NULL, // wpa2EntSetCertKey,
-  NULL, // wpa2EntEnable,
+    // 0x40 -> 0x4f
+    NULL,  // setClientCert,
+    NULL,  // setCertKey,
+    NULL, NULL,
+    NULL,  // sendDataTcp,
+    NULL,  // getDataBufTcp,
+    NULL,  // insertDataBuf,
+    NULL, NULL, NULL,
+    NULL,  // wpa2EntSetIdentity,
+    NULL,  // wpa2EntSetUsername,
+    NULL,  // wpa2EntSetPassword,
+    NULL,  // wpa2EntSetCACert,
+    NULL,  // wpa2EntSetCertKey,
+    NULL,  // wpa2EntEnable,
 
-  // 0x50 -> 0x5f
-  NULL, // setPinMode,
-  NULL, // setDigitalWrite,
-  NULL, // setAnalogWrite,
-  NULL, // setDigitalRead,
-  NULL, // setAnalogRead,
-  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    // 0x50 -> 0x5f
+    NULL,  // setPinMode,
+    NULL,  // setDigitalWrite,
+    NULL,  // setAnalogWrite,
+    NULL,  // setDigitalRead,
+    NULL,  // setAnalogRead,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
 
-  // 0x60 -> 0x6f: Bluepad32 own extensions
-  request_connected_gamepads, // request_connected_gamepads
-  request_gamepad_data, // request_gamepad_data
-  NULL, // request_set_gamepad_rumble
-  NULL, // request_set_gamepad_led_color
+    // 0x60 -> 0x6f: Bluepad32 own extensions
+    request_connected_gamepads,  // request_connected_gamepads
+    request_gamepad_data,        // request_gamepad_data
+    NULL,                        // request_set_gamepad_rumble
+    NULL,                        // request_set_gamepad_led_color
 };
+#define COMMAND_HANDLERS_MAX \
+  (sizeof(command_handlers) / sizeof(command_handlers[0]))
 
-static int process_request(const uint8_t command_buf[], int command_len,
-                           uint8_t out[]) {
-  return 0;
+static int process_request(const uint8_t command[], int command_len,
+                           uint8_t response[] /* out */) {
+  if (command_len < 2) {
+    loge("Airlift: invalid process_request lenght: got %d, want >= 2",
+         command_len);
+    return -1;
+  }
+  int response_len = 0;
+
+  if (command[0] == 0xe0 && command[1] < COMMAND_HANDLERS_MAX) {
+    command_handler_t command_handler = command_handlers[command[1]];
+
+    if (command_handler) {
+      response_len = command_handler(command, response);
+    }
+  }
+
+  if (response_len <= 0) {
+    response[0] = 0xef;
+    response[1] = 0x00;
+    response[2] = 0xee;
+
+    response_len = 3;
+  } else {
+    response[0] = 0xe0;
+    response[1] = (0x80 | command[1]);
+    response[response_len - 1] = 0xee;
+  }
+
+  return response_len;
 }
 
 #define SPI_BUFFER_LEN SPI_MAX_DMA_LEN
@@ -220,7 +249,8 @@ static void spi_post_setup_cb(spi_slave_transaction_t* trans) {
   // Create SPI main loop thread
   // Bluetooth code runs in Core 0.
   // In order to not interfere with it, this one should run in the other Core.
-  xTaskCreatePinnedToCore(spi_main_loop, "spi_main_loop", 8192, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(spi_main_loop, "spi_main_loop", 8192, NULL, 1, NULL,
+                          1);
 }
 
 static void IRAM_ATTR isr_handler_on_chip_select(void* arg) {
@@ -279,7 +309,7 @@ static void airlift_on_init_complete(void) {}
 static void airlift_on_port_assign_changed(uni_joystick_port_t port) {}
 
 static void airlift_on_mouse_data(int32_t delta_x, int32_t delta_y,
-                                        uint16_t buttons) {}
+                                  uint16_t buttons) {}
 
 static void airlift_on_joy_a_data(uni_joystick_t* joy) {}
 
