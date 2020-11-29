0d9bef2d85c05af632bb9b7d72cf3fe00d395da4
Ricardo Quesada <ricardoquesada@gmail.com>
2020-11-28 18:34:01 -0800

    circuitpython snake: kind of working

--- tools/circuitpython/code_snake.py
+++ tools/circuitpython/code_snake.py
@@ -10,6 +10,7 @@
 
 import math
 import time
+import random
 
 import bluepad32
 
@@ -20,16 +21,121 @@ from adafruit_matrixportal.matrix import Matrix
 from digitalio import DigitalInOut
 from micropython import const
 
+# Global constants
+SCREEN_WIDTH = const(64)
+SCREEN_HEIGHT = const(32)
+PALETTE_SIZE = const(16)
 
-class Paint:
-    _SCREEN_WIDTH = const(64)
-    _SCREEN_HEIGHT = const(32)
-    _PALETTE_SIZE = const(16)
+class Fruit:
+    def __init__(self, pos):
+        self._pos = pos
+        self._color = 4
+        x, y = pos[0], pos[1]
+        self._pixels = [(pos), (x+1, y), (x, y+1), (x+1, y+1)]
 
+    def animation(self):
+        pass
+
+    def draw(self, bitmap):
+        for p in self._pixels:
+            bitmap[p[0], p[1]] = self._color
+
+    def pixels(self):
+        return self._pixels
+
+class Snake:
+    def __init__(self, snake, direction, color):
+        self._direction = direction
+        self._snake = snake
+        self._len = len(snake)
+        self._color = color
+
+    def set_direction(self, direction) -> None:
+        self._direction = direction
+
+    def increase_tail(self, units: int) -> None:
+        self._len += units
+
+    def eat_fruit(self, fruit: Fruit) -> bool:
+        head = self._snake[0]
+        return head in fruit.pixels()
+
+    def animate(self) -> int:
+        head = self._snake[0]
+        # x
+        x = self._direction[0] + head[0]
+        # y
+        y = self._direction[1] + head[1]
+        if x < 0 or x >= SCREEN_WIDTH:
+            return -1
+        if y < 0 or y >= SCREEN_HEIGHT:
+            return -1
+        self._snake.insert(0, (x,y))
+        if len(self._snake) > self._len:
+            # Remove tail
+            del self._snake[-1]
+        return 0
+
+    def draw(self, bitmap) -> None:
+        for s in self._snake:
+            bitmap[s[0], s[1]] = self._color
+
+class Display:
     def __init__(self):
-        self._esp = self._init_spi()
         self._bitmap, self._palette = self._init_bitmap()
         self._display = self._init_display(self._bitmap, self._palette)
+
+    def get_bitmap(self):
+        return self._bitmap
+
+    def _init_bitmap(self):
+        bitmap = displayio.Bitmap(SCREEN_WIDTH, SCREEN_HEIGHT, PALETTE_SIZE)
+
+        # For fun, just use the CGA color palette
+        palette = displayio.Palette(PALETTE_SIZE)
+        palette[0] = (0, 0, 0)  # Black
+        palette[1] = (0, 0, 170)  # Blue
+        palette[2] = (0, 170, 0)  # Green
+        palette[3] = (0, 170, 170)  # Cyan
+        palette[4] = (170, 0, 0)  # Red
+        palette[5] = (170, 0, 170)  # Magenta
+        palette[6] = (170, 85, 0)  # Brown
+        palette[7] = (170, 170, 170)  # Light Gray
+
+        palette[8] = (85, 85, 85)  # Dark Gray
+        palette[9] = (85, 85, 255)  # Light Blue
+        palette[10] = (85, 255, 85)  # Light Green
+        palette[11] = (85, 255, 255)  # Light Cyan
+        palette[12] = (255, 85, 85)  # Light Red
+        palette[13] = (255, 85, 255)  # Light Magenta
+        palette[14] = (255, 255, 85)  # Yellow
+        palette[15] = (255, 255, 255)  # White
+
+        return bitmap, palette
+
+    def _init_display(self, bitmap, palette):
+        tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette)
+        group = displayio.Group()
+        group.append(tile_grid)
+        # 16 colors == 4 bit of depth
+        matrix = Matrix(bit_depth=math.ceil(math.log(PALETTE_SIZE, 2)))
+        display = matrix.display
+        display.auto_refresh = True
+        display.rotation = 0
+        display.show(group)
+
+        return display
+
+    def refresh(self):
+        self._display.refresh()
+        #print(dir(self._display))
+        #print(dir(self._display.framebuffer))
+        #xxx
+
+class Game:
+    def __init__(self):
+        self._esp = self._init_spi()
+        self._display = Display()
 
     def _init_spi(self):
         # If you are using a board with pre-defined ESP32 Pins:
@@ -58,51 +164,15 @@ class Paint:
             spi, esp32_cs, esp32_ready, esp32_reset, debug=0)
         return esp
 
-    def _init_bitmap(self):
-        bitmap = displayio.Bitmap(_SCREEN_WIDTH, _SCREEN_HEIGHT, _PALETTE_SIZE)
-
-        # For fun, just use the CGA color palette
-        palette = displayio.Palette(_PALETTE_SIZE)
-        palette[0] = (0, 0, 0)  # Black
-        palette[1] = (0, 0, 170)  # Blue
-        palette[2] = (0, 170, 0)  # Green
-        palette[3] = (0, 170, 170)  # Cyan
-        palette[4] = (170, 0, 0)  # Red
-        palette[5] = (170, 0, 170)  # Magenta
-        palette[6] = (170, 85, 0)  # Brown
-        palette[7] = (170, 170, 170)  # Light Gray
-
-        palette[8] = (85, 85, 85)  # Dark Gray
-        palette[9] = (85, 85, 255)  # Light Blue
-        palette[10] = (85, 255, 85)  # Light Green
-        palette[11] = (85, 255, 255)  # Light Cyan
-        palette[12] = (255, 85, 85)  # Light Red
-        palette[13] = (255, 85, 255)  # Light Magenta
-        palette[14] = (255, 255, 85)  # Yellow
-        palette[15] = (255, 255, 255)  # White
-
-        return bitmap, palette
-
-    def _init_display(self, bitmap, palette):
-        tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette)
-        group = displayio.Group()
-        group.append(tile_grid)
-        # 16 colors == 4 bit of depth
-        matrix = Matrix(bit_depth=math.ceil(math.log(_PALETTE_SIZE, 2)))
-        display = matrix.display
-        display.show(group)
-
-        return display
-
     def run(self):
         # esp.set_esp_debug(1)
-        x = _SCREEN_WIDTH // 2
-        y = _SCREEN_HEIGHT // 2
-        color = 1
+        x = SCREEN_WIDTH // 2
+        y = SCREEN_HEIGHT // 2
 
-        snake = [(x, y)]
-        dir_x = -1
-        dir_y = 0
+        snake0 = Snake([(x//2, y), (x//2+1,y), (x//2+2,y)], direction=(-1,0), color=1)
+        fruit = Fruit(pos=(random.randint(0,SCREEN_WIDTH-2), random.randint(0,SCREEN_HEIGHT-2)))
+
+        bitmap = self._display.get_bitmap()
 
         while True:
             # TODO: Is there a "displayio.wait_for_refresh()". Couldn't find it.
@@ -115,29 +185,32 @@ class Paint:
                 # d-pad constants are defined here:
                 # https://gitlab.com/ricardoquesada/bluepad32/-/blob/master/src/main/uni_gamepad.h
                 dpad = gp["dpad"]
-                if dpad & (0x01 | 0x02):  # Up or Down
-                    dir_y = -1 if dpad & 0x01 else 1
-                    dir_x = 0
-                elif dpad & (0x04 | 0x08): # Right or Left
-                    dir_x = 1 if dpad & 0x04 else -1
-                    dir_y = 0
+                needs_update = True
+                if dpad & 0x01: # Up
+                    dir_x, dir_y = 0, -1
+                elif dpad & 0x02: # Down
+                    dir_x, dir_y = 0, 1
+                elif dpad & 0x04: # Right
+                    dir_x, dir_y = 1, 0
+                elif dpad & 0x08: # Left
+                    dir_x, dir_y = -1, 0
+                else:
+                    needs_update = False
 
-            head = snake[0]
-            new_x = head[0] + dir_x
-            new_x = max(0, new_x)
-            new_x = min(_SCREEN_WIDTH-1, new_x)
+                if needs_update:
+                    snake0.set_direction((dir_x, dir_y))
 
-            new_y = head[1] + dir_y
-            new_y = max(0, new_y)
-            new_y = min(_SCREEN_HEIGHT-1, new_y)
+            snake0.animate()
 
-            snake.insert(0, (new_x, new_y))
-            if len(snake) > 10:
-                del snake[-1]
+            # Clear screen + draw different sprites
+            bitmap.fill(0)
+            snake0.draw(bitmap)
+            fruit.draw(bitmap)
 
-            self._bitmap.fill(0)
-            for s in snake:
-                self._bitmap[s[0], s[1]] = color
+            # check collision
+            if snake0.eat_fruit(fruit):
+                fruit = Fruit(pos=(random.randint(0,SCREEN_WIDTH-2), random.randint(0,SCREEN_HEIGHT-2)))
+                snake0.increase_tail(1)
 
-
-Paint().run()
+            self._display.refresh()
+Game().run()
