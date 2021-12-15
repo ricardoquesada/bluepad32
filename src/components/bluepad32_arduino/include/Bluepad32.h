17e290788abee9f83d691008ecb9d3aae8de3f21
Ricardo Quesada <ricardoquesada@gmail.com>
2021-12-15 10:26:59 -0800

    Build system: updates to make it work on Arduino
    Minor fixes to make component.mk and CMakeLists.txt compile with Arduino
    component.
    
    Also, moves the bluepad32_arduino headers to include/
    This is to be consistent with bluepad32/ and also, the default is have
    to the headers in include/

--- src/components/bluepad32_arduino/include/Bluepad32.h
+++ src/components/bluepad32_arduino/include/Bluepad32.h
@@ -1,10 +1,10 @@
 // Copyright 2021 - 2021, Ricardo Quesada, http://retro.moe
 // SPDX-License-Identifier: Apache-2.0 or LGPL-2.1-or-later
 
-#ifndef BP32_GAMEPAD_H
-#define BP32_GAMEPAD_H
+#ifndef BP32_BLUEPAD32_H
+#define BP32_BLUEPAD32_H
 
 #include "ArduinoBluepad32.h"
 #include "ArduinoGamepad.h"
 
-#endif  // BP32_GAMEPAD_H
+#endif  // BP32_BLUEPAD32_H
