dd33a60ed6cedb5532798386bed2cf47419c2cc9
Ricardo Quesada <ricardoquesada@gmail.com>
2023-08-20 07:51:29 -0700

    Add missing files

---
+++ src/components/bluepad32/include/uni_error.h
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2023 Ricardo Quesada
+// http://retro.moe/unijoysticle2
+
+#ifndef UNI_ERROR_H
+#define UNI_ERROR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    UNI_ERROR_SUCCESS = 0,
+
+    UNI_ERROR_INVALID_DEVICE = 1,
+    UNI_ERROR_INVALID_CONTROLLER = 2,
+    UNI_ERROR_NO_SLOTS = 3,
+} uni_error_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // UNI_ERROR_H
