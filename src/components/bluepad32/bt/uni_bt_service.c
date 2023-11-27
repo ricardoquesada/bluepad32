6330635674e5dadb13618aeca125a0f557254220
Ricardo Quesada <ricardoquesada@gmail.com>
2023-11-27 07:40:54 -0800

    clang-format fixes

--- src/components/bluepad32/bt/uni_bt_service.c
+++ src/components/bluepad32/bt/uni_bt_service.c
@@ -2,14 +2,11 @@
 // Copyright 2023 Ricardo Quesada
 // http://retro.moe/unijoysticle2
 
-
 #include "bt/uni_bt_service.h"
 
 static bool service_enabled;
 
-void uni_bt_service_init(void) {
-
-}
+void uni_bt_service_init(void) {}
 
 void uni_bt_service_set_enabled(bool enabled) {
     service_enabled = enabled;
