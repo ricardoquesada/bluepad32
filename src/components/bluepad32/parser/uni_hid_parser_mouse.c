99a074badf942609cd54ca2474d64371d699ef96
Ricardo Quesada <ricardoquesada@gmail.com>
2023-12-24 09:04:47 -0800

    .gatt file is generated manually, not at compile time...
    ... to reduce dependencies in projects that use Bluepad32

--- src/components/bluepad32/parser/uni_hid_parser_mouse.c
+++ src/components/bluepad32/parser/uni_hid_parser_mouse.c
@@ -1,24 +1,6 @@
-/****************************************************************************
-http://retro.moe/unijoysticle2
-
-Copyright 2019 Ricardo Quesada
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-****************************************************************************/
-
-// Generic gamepads. Gamepads that were desigened to be "multi console, and
-// might implemenet usages that are invalid for specific consoles. In order to
-// keep clean the pure-console implementations, add here the generic ones.
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2019 Ricardo Quesada
+// http://retro.moe/unijoysticle2
 
 #include "parser/uni_hid_parser_mouse.h"
 
--- src/components/bluepad32/platform/uni_platform_unijoysticle.c
+++ src/components/bluepad32/platform/uni_platform_unijoysticle.c
@@ -1,30 +1,14 @@
-#include <sys/cdefs.h>
-#include <sys/queue.h>
-/****************************************************************************
-http://retro.moe/unijoysticle2
-
-Copyright 2019 Ricardo Quesada
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-****************************************************************************/
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2019 Ricardo Quesada
+// http://retro.moe/unijoysticle2
 
 // Unijoysticle platform
 
 #include "platform/uni_platform_unijoysticle.h"
 
-#include <inttypes.h>
 #include <math.h>
 #include <stdbool.h>
+#include <sys/cdefs.h>
 
 #include <argtable3/argtable3.h>
 #include <driver/gpio.h>
