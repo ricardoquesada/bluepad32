fa24b691e61fe8e9bf24254d7fe27ba9c11ddecf
Ricardo Quesada <ricardoquesada@gmail.com>
2024-06-03 14:21:30 -0700

    add "number" version constants

--- src/components/bluepad32/include/uni_version.h
+++ src/components/bluepad32/include/uni_version.h
@@ -5,7 +5,13 @@
 #ifndef UNI_VERSION_H
 #define UNI_VERSION_H
 
+// String version
 #define UNI_VERSION "4.1.0"
+
+// Number version, in case a 3rd party needs to check it
+#define UNI_VERSION_MAJOR 4
+#define UNI_VERSION_MINOR 1
+#define UNI_VERSION_PATCH 0
 
 extern const char* uni_version;
 
