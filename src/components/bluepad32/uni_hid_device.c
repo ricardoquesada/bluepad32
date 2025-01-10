d9fc70130414436ab5189322064e322fde10d475
Ricardo Quesada <ricardoquesada@gmail.com>
2025-01-09 17:58:37 -0800

    remove dump_all() when processing "misc buttons"
    each platform should be responsible for that

--- src/components/bluepad32/uni_hid_device.c
+++ src/components/bluepad32/uni_hid_device.c
@@ -963,8 +963,6 @@ static void process_misc_button_home(uni_hid_device_t* d) {
 
     // Update "wait" flag.
     d->misc_button_wait_release |= MISC_BUTTON_START;
-
-    uni_hid_device_dump_all();
 }
 
 static void device_connection_timeout(btstack_timer_source_t* ts) {
